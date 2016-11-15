fc95ddd6546997748e93e735064ca00472021253
bwithbroe <withbroeb@carleton.edu>
2016-11-14 22:10:36 -0600

    ensure pythag_thm only accepts numeric values

--- sdcUtils.hh
+++ sdcUtils.hh
@@ -1,4 +1,11 @@
+#include <type_traits>
+#include <math.h>
+
+// computes the pythagorean theoram (for distance calculation), and only
+// accepts numeric types.
 template<typename T>
 T pythag_thm(T x, T y) {
+  static_assert(std::is_arithmetic<T>::value, "pythag_thm only accepts numeric values");
   return sqrt(pow(x, 2) + pow(y, 2));
 }
+
