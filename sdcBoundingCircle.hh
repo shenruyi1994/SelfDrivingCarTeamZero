7c2d2f7c0890dd6b6199d53306a20d17069297d0
bwithbroe <withbroeb@carleton.edu>
2016-11-21 01:08:39 -0600

    add sdcBoundingCircle header file

---
+++ sdcBoundingCircle.hh
@@ -0,0 +1,27 @@
+#ifndef _sdcBoundingCircle_hh
+#define _sdcBoundingCircle_hh
+
+#include <opencv2/opencv.hpp>
+
+#include "sdcAngle.hh"
+#include "sdcBoundingShape.hh"
+#include "sdcBoundingBox.hh"
+
+namespace gazebo {
+  class sdcBoundingCircle: public sdcBoundingShape {
+  public:
+    sdcBoundingCircle(double x, double y, double radius): radius(radius) {
+      center = cv::Point2d(x, y);
+    }
+    sdcBoundingCircle(const Point2d& center, double radius):
+      centerpoint_(center), radius(radius) {}
+
+    bool DoesIntersect(const sdcBoundingCircle& box) const;
+    bool DoesIntersect(const sdcBoundingBox& box) const;
+
+    cv::Point2d center;
+    double radius;
+  };
+}
+
+#endif
