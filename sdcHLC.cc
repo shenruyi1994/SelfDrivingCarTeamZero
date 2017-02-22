5b437a0ad3ef6f836a27b28c8632b1b40dd364a8
choitom <sdcar@eduroam-165-74.dyn.carleton.edu>
2017-02-21 20:37:01 -0600

    doubled the speed to 10

--- sdcHLC.cc
+++ sdcHLC.cc
@@ -249,7 +249,7 @@ void sdcHLC::WaypointDriving(std::vector<sdcWaypoint> WAYPOINT_VEC) {
  * along sharp turns in the curve.
  */
 void sdcHLC::FollowWaypoints() {
-  car_->SetTargetSpeed(5);
+  car_->SetTargetSpeed(10);
 
   cv::Point2d targetPoint = FindDubinsTargetPoint();
   printf("targetPoint: (3%f, %f)\n", targetPoint.x, targetPoint.y);
